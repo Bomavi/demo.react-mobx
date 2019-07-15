6ee86bfb49d2aa0c1a25f9f04f7228dff8840cc6
Bomavi <max.bozh.n1mbo@gmail.com>
2019-07-16 01:54:58 +0300

    Add theme manipulations to GlobalStore

--- src/config/store.ts
+++ src/config/store.ts
@@ -1,9 +1,30 @@
-import { observable, action } from 'mobx';
+/* npm imports: common */
+import { observable, computed, action } from 'mobx';
+
+/* npm imports: material-ui/core */
+import { Theme } from '@material-ui/core/styles';
+
+/* root imports: common */
+import { lightTheme, darkTheme } from 'utils/themes';
 
 export class GlobalStore {
-	@observable public isLight: boolean = true;
-	@action public switchTheme = (): void => {
-		this.isLight = !this.isLight;
+	@observable public isDark: boolean = false;
+	@observable public isDrawerOpen: boolean = false;
+
+	@computed public get themeNameToSwitch() {
+		return this.isDark ? 'light' : 'dark';
+	}
+
+	@computed public get selectedTheme(): Theme {
+		return this.isDark ? darkTheme : lightTheme;
+	}
+
+	@action public switchTheme = () => {
+		this.isDark = !this.isDark;
+	};
+
+	@action public toggleDrawer = () => {
+		this.isDrawerOpen = !this.isDrawerOpen;
 	};
 }
 
