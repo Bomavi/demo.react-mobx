59fb27c40c88ca3c504d44fcda911f364be4ee8c
Bomavi <max.bozh.n1mbo@gmail.com>
2019-07-17 23:32:14 +0300

    Change Home content and structure

--- src/features/Home/index.tsx
+++ src/features/Home/index.tsx
@@ -1,17 +1,12 @@
 /* npm imports: common */
 import React from 'react';
 import { Provider, observer, inject } from 'mobx-react';
-import styled from 'styled-components';
+
+/* root imports: view components */
+import { ActionTabs } from 'views/layouts';
 
 /* local imports: common */
 import { homeStore, HomeStore } from './store';
-
-const PageWrapper = styled.div`
-	position: absolute;
-	top: 50%;
-	left: 50%;
-	transform: translate(-50%, -50%);
-`;
 
 interface HomePageProps {
 	store?: HomeStore;
@@ -23,9 +18,9 @@ class HomePage extends React.Component<HomePageProps> {
 	public render() {
 		// const { name } = this.props.store!;
 		return (
-			<PageWrapper>
-				<h1>Home Page</h1>
-			</PageWrapper>
+			<div>
+				<ActionTabs />
+			</div>
 		);
 	}
 }
