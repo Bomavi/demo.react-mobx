8a6d17f6e24c4ed32ee861c57344618a5c6eb35a
Bomavi <max.bozh.n1mbo@gmail.com>
2019-07-17 23:27:17 +0300

    Add debounce helper

---
+++ src/utils/helpers/debounce/index.ts
@@ -0,0 +1,16 @@
+export const debounce = (func: Function, wait: number, immediate?: boolean) => {
+	let timeout: ReturnType<typeof setTimeout> | null;
+
+	return function() {
+		const args = arguments;
+		const later = () => {
+			timeout = null;
+			if (!immediate) func.apply(this, args);
+		};
+		const callNow = immediate && !timeout;
+
+		if (timeout) clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(this, args);
+	};
+};
