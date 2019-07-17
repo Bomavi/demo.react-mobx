1ac7c10b4be685ab773932fb2769f9bd6bd1d82d
Bomavi <max.bozh.n1mbo@gmail.com>
2019-07-17 23:31:37 +0300

    Add new layouts

--- src/views/layouts/index.ts
+++ src/views/layouts/index.ts
@@ -3,3 +3,6 @@ export * from './Header';
 export * from './Footer';
 export * from './Content';
 export * from './Drawer';
+export * from './ActionTabs';
+export * from './AddTask';
+export * from './Search';
