436b6da0f57a621025367aa2d1ff3379924c607a
Bomavi <max.bozh.n1mbo@gmail.com>
2019-07-20 04:05:19 +0300

    Change theme gradiensts primary color

--- src/utils/themes/default.ts
+++ src/utils/themes/default.ts
@@ -4,7 +4,7 @@ import { createMuiTheme } from '@material-ui/core/styles';
 /* NOTE: usage: { propName: defaultTheme.propName } */
 export const defaultTheme = createMuiTheme({
 	gradients: {
-		primary: 'linear-gradient(45deg, #2196F3 30%, #21CBF3 90%)',
+		primary: 'linear-gradient(45deg, #26a69a 30%, #66bb6a 90%)',
 		secondary: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
 	},
 });
