da9c79cbd0b634db91556e715974cb2a0924afa9
Bomavi <max.bozh.n1mbo@gmail.com>
2019-07-24 23:57:47 +0300

    Add login typings

--- src/typings/features/login.d.ts
+++ src/typings/features/login.d.ts
@@ -1,10 +1,20 @@
 declare interface LoginType {
-	username: string;
-	password: string;
-	isGuest: boolean;
+	username?: string;
+	password?: string;
+	isGuest?: boolean;
 }
 
 declare interface RegisterType {
 	username: string;
 	password: string;
 }
+
+declare interface UserType {
+	username: string;
+	createdAt: string;
+	updatedAt: string;
+}
+
+declare type AuthenticateResponseType = UserType | null;
+declare type LoginResponseType = UserType | null;
+declare type RegisterResponseType = UserType | null;
