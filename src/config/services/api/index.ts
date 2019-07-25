c2ef2f8bbd8b27f3111da442bee06ea7207bb6bf
Bomavi <max.bozh.n1mbo@gmail.com>
2019-07-25 23:47:29 +0300

    Add UsersApi and modify user.model

--- src/config/services/api/index.ts
+++ src/config/services/api/index.ts
@@ -3,7 +3,9 @@ import { Base } from 'config/axios/base';
 
 /* local imports: common */
 import { TasksApi } from './tasks';
+import { UsersApi } from './users';
 
 export class ApiService extends Base {
 	public tasks: TasksApi = new TasksApi(this.apiClient);
+	public users: UsersApi = new UsersApi(this.apiClient);
 }
--- src/features/Login/user.model.ts
+++ src/features/Login/user.model.ts
@@ -1,18 +1,20 @@
 /* npm imports: common */
-import { computed } from 'mobx';
+// import { computed } from 'mobx';
 
 export class UserModel {
+	public readonly id: string = '';
 	public readonly username: string = '';
 	public readonly createdAt: string = '';
 	public readonly updatedAt: string = '';
 
 	public constructor(props: Partial<UserType>) {
+		if (props._id) this.id = props._id;
 		if (props.username) this.username = props.username;
 		if (props.createdAt) this.createdAt = props.createdAt;
 		if (props.updatedAt) this.updatedAt = props.updatedAt;
 	}
 
-	@computed public get isAuthenticated() {
-		return !!this.username;
-	}
+	// @computed public get created_at() {
+	// 	return moment(this.createdAt).format();
+	// }
 }
