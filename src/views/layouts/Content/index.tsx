64662880fb507ef0efc79be07cc386cbcbed6969
Bomavi <max.bozh.n1mbo@gmail.com>
2019-07-25 22:21:12 +0300

    Inject AuthStore and check if authenticated

--- src/views/layouts/Content/index.tsx
+++ src/views/layouts/Content/index.tsx
@@ -1,24 +1,42 @@
 /* npm imports: common */
 import React from 'react';
+import { observer, inject } from 'mobx-react';
+
+/* npm imports: material-ui/core */
+import { withStyles, WithStyles } from '@material-ui/core/styles';
 
 /* root imports: view components */
 import { Header, Footer, Drawer } from 'views/layouts';
 
+/* root imports: common */
+import { AuthStore } from 'features/Login/store';
+
 /* local imports: common */
-import { useStyles } from './styles';
+import { styles } from './styles';
 
-const Content: React.FC = ({ children }) => {
-	const classes = useStyles();
+interface ContentProps extends WithStyles<typeof styles> {
+	authStore?: AuthStore;
+}
 
-	return (
-		<>
-			<Header />
-			<div className={classes.toolbar} />
-			<Drawer />
-			<main className={classes.root}>{children}</main>
-			<Footer />
-		</>
-	);
-};
+@inject('authStore')
+@observer
+class ContentComponent extends React.Component<ContentProps> {
+	public render() {
+		const { children, classes } = this.props;
+		const { isAuthenticated } = this.props.authStore!;
+
+		return (
+			<>
+				<Header />
+				<div className={classes.toolbar} />
+				{isAuthenticated && <Drawer />}
+				<main className={classes.root}>{children}</main>
+				<Footer />
+			</>
+		);
+	}
+}
+
+const Content = withStyles(styles)(ContentComponent);
 
 export { Content };
