da9ca7cb2d9e905a3298a223383f091472655d23
Bomavi <max.bozh.n1mbo@gmail.com>
2019-07-26 20:11:19 +0300

    Modify from FC to CC and inject store

--- src/features/Home/index.tsx
+++ src/features/Home/index.tsx
@@ -1,24 +1,34 @@
 /* npm imports: common */
 import React from 'react';
-import { Provider } from 'mobx-react';
+import { Provider, inject, observer } from 'mobx-react';
 
 /* npm imports: material-ui/core */
 import Container from '@material-ui/core/Container';
 
 /* local imports: common */
 import { ActionTabs, TaskList } from './components';
-import { homeStore } from './store';
+import { homeStore, HomeStore } from './store';
 
-interface HomePageProps {}
+interface HomePageProps {
+	store?: HomeStore;
+}
 
-const HomePage: React.FC<HomePageProps> = () => {
-	return (
-		<Container maxWidth="md">
-			<ActionTabs />
-			<TaskList />
-		</Container>
-	);
-};
+@inject('store')
+@observer
+class HomePage extends React.Component<HomePageProps> {
+	public componentDidMount() {
+		this.props.store!.searchTasks();
+	}
+
+	public render() {
+		return (
+			<Container maxWidth="md">
+				<ActionTabs />
+				<TaskList />
+			</Container>
+		);
+	}
+}
 
 export const Home = () => (
 	<Provider store={homeStore}>
