847f290e0fdace45169dc6d531c0ed8a9c100ed8
Bomavi <max.bozh.n1mbo@gmail.com>
2019-07-26 15:49:57 +0300

    Change AUTH_PATH from "/auth" to  "/api/auth"

--- src/utils/constants/index.ts
+++ src/utils/constants/index.ts
@@ -5,8 +5,8 @@ export const HOST = process.env.REACT_APP_DEV_HOST || '';
 export const SERVICES_PATH = process.env.REACT_APP_SERVICES_PATH;
 export const SERVICES_URL = `${HOST}${SERVICES_PATH}`;
 
-export const AUTH_PATH = process.env.REACT_APP_AUTH_PATH;
-export const AUTH_URL = `${SERVICES_URL}${AUTH_PATH}`;
-
 export const API_PATH = process.env.REACT_APP_API_PATH;
 export const API_URL = `${SERVICES_URL}${API_PATH}`;
+
+export const AUTH_PATH = process.env.REACT_APP_AUTH_PATH;
+export const AUTH_URL = `${API_URL}${AUTH_PATH}`;
