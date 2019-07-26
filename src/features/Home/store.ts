1ccc453642954e4a48084a208b19f98b89d06683
Bomavi <max.bozh.n1mbo@gmail.com>
2019-07-26 22:03:13 +0300

    Modify HomeStore and cleanup

--- src/features/Home/store.ts
+++ src/features/Home/store.ts
@@ -1,14 +1,15 @@
 /* npm imports: common */
-import { observable, computed, action } from 'mobx';
+import { observable, computed, action, IObservableArray } from 'mobx';
 
 /* root imports: common */
 import { BaseStore } from 'config/base-store';
 
 /* local imports: common */
 import { Search } from './search.model';
+import { TaskModel } from './task.model';
 
 export class HomeStore extends BaseStore {
-	@observable public tasks: string[] = [];
+	@observable public tasks: IObservableArray<TaskModel> = observable([]);
 
 	@observable public search: Search = new Search();
 
@@ -16,20 +17,37 @@ export class HomeStore extends BaseStore {
 		return this.tasks.length;
 	}
 
-	@computed public get fetchParams() {
+	@computed public get tasksList() {
+		return this.tasks.slice();
+	}
+
+	@computed private get fetchParams() {
 		return {
 			...this.search.toJSON(),
 		};
 	}
 
-	@action private setTasks = (tasks: any) => {
-		this.tasks = tasks;
+	@action private setTasks = (tasks: TaskType[]) => {
+		this.tasks.replace(tasks.map(t => new TaskModel(t)));
 	};
 
-	public searchTasks = () => {
+	@action private setTask = (task: TaskType) => {
+		this.tasks.replace([...this.tasks, new TaskModel(task)]);
+	};
+
+	public searchTasks = async () => {
 		try {
-			const tasks = this.services.api.tasks.search(this.fetchParams);
+			const tasks = await this.services.api.tasks.search(this.fetchParams);
 			this.setTasks(tasks);
+		} catch (e) {
+			console.error(e);
+		}
+	};
+
+	public addTask = async (taskData: TaskUpdateSchema) => {
+		try {
+			const task = await this.services.api.tasks.create(taskData);
+			this.setTask(task);
 		} catch (e) {
 			console.error(e);
 		}
