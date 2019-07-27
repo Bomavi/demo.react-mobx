09b76d55803a4e0df5f6b84287035308bbbe6ea3
Bomavi <max.bozh.n1mbo@gmail.com>
2019-07-27 23:42:00 +0300

    Extends BaseStore and add progress states

--- src/features/Home/task.model.ts
+++ src/features/Home/task.model.ts
@@ -1,7 +1,9 @@
 /* npm imports: common */
-// import { computed } from 'mobx';
+import { observable, action } from 'mobx';
 
-export class TaskModel {
+import { BaseStore } from 'config/base-store';
+
+export class TaskModel extends BaseStore {
 	public readonly id: string = '';
 	public readonly description: string = '';
 	public readonly completed: boolean = false;
@@ -9,6 +11,8 @@ export class TaskModel {
 	public readonly updatedAt: string = '';
 
 	public constructor(props: Partial<TaskType>) {
+		super();
+
 		if (props._id) this.id = props._id;
 		if (props.description) this.description = props.description;
 		if (props.completed) this.completed = props.completed;
@@ -16,7 +20,19 @@ export class TaskModel {
 		if (props.updatedAt) this.updatedAt = props.updatedAt;
 	}
 
+	@observable public updateInProgress: boolean = false;
+	@observable public deleteInProgress: boolean = false;
+
 	// @computed public get created_at() {
 	// 	return moment(this.createdAt).format();
 	// }
+
+	@action public setActionInProgress = (action: TaskActionInProgress, state: boolean) => {
+		try {
+			if (!action) throw Error('action not found');
+			this[action] = state;
+		} catch (e) {
+			console.error(e);
+		}
+	};
 }
