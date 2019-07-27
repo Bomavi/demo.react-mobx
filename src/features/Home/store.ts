ed2eb96db9bab1c05de6b53b244b88cee5692016
Bomavi <max.bozh.n1mbo@gmail.com>
2019-07-27 23:42:45 +0300

    Cleanup, add isFetching state and modify typings

--- src/features/Home/store.ts
+++ src/features/Home/store.ts
@@ -11,13 +11,16 @@ import { TaskModel } from './task.model';
 export class HomeStore extends BaseStore {
 	@observable public tasks: IObservableArray<TaskModel> = observable([]);
 
-	@observable public search: Search = new Search();
+	@observable public isFetching: boolean = false;
+	@observable public inProgress: boolean = false;
 
-	@computed public get tasksLength() {
-		return this.tasks.length;
+	@observable.ref public readonly search: Search = new Search();
+
+	@computed public get isEmpty() {
+		return this.tasks.length === 0;
 	}
 
-	@computed public get tasksList() {
+	@computed public get taskList() {
 		return this.tasks.slice();
 	}
 
@@ -27,6 +30,15 @@ export class HomeStore extends BaseStore {
 		};
 	}
 
+	@action public setFetchingState = (action: TaskFetchingState, state: boolean) => {
+		try {
+			if (!action) throw Error('action not found');
+			this[action] = state;
+		} catch (e) {
+			console.error(e);
+		}
+	};
+
 	@action private setTasks = (tasks: TaskType[]) => {
 		this.tasks.replace(tasks.map(t => new TaskModel(t)));
 	};
@@ -35,19 +47,51 @@ export class HomeStore extends BaseStore {
 		this.tasks.replace([...this.tasks, new TaskModel(task)]);
 	};
 
+	@action private unsetTask = (id: string) => {
+		this.tasks.replace(this.taskList.filter(t => t.id !== id));
+	};
+
+	@action private replaceTask = (task: TaskType) => {
+		this.tasks.replace(this.taskList.map(t => (t.id === task._id ? new TaskModel(task) : t)));
+	};
+
 	public searchTasks = async () => {
 		try {
+			this.setFetchingState('isFetching', true);
 			const tasks = await this.services.api.tasks.search(this.fetchParams);
 			this.setTasks(tasks);
 		} catch (e) {
 			console.error(e);
+		} finally {
+			this.setFetchingState('isFetching', false);
 		}
 	};
 
 	public addTask = async (taskData: TaskUpdateSchema) => {
 		try {
+			this.setFetchingState('inProgress', true);
 			const task = await this.services.api.tasks.create(taskData);
 			this.setTask(task);
+		} catch (e) {
+			console.error(e);
+		} finally {
+			this.setFetchingState('inProgress', false);
+		}
+	};
+
+	public updateTask = async (id: string, taskData: TaskUpdateSchema) => {
+		try {
+			const task = await this.services.api.tasks.update(id, taskData);
+			this.replaceTask(task);
+		} catch (e) {
+			console.error(e);
+		}
+	};
+
+	public deleteTask = async (id: string) => {
+		try {
+			await this.services.api.tasks.delete(id);
+			this.unsetTask(id);
 		} catch (e) {
 			console.error(e);
 		}
