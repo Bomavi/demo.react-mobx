e3a6203869858e5131e2d37c34fe46558cdd4836
Bomavi <max.bozh.n1mbo@gmail.com>
2019-07-29 01:18:28 +0300

    Add DrawerItem

--- src/views/layouts/Drawer/Logout/index.tsx
+++ src/views/layouts/Drawer/Logout/index.tsx
@@ -1,16 +1,9 @@
 /* npm imports: common */
 import React from 'react';
 import { observer, inject } from 'mobx-react';
-import { observable, action } from 'mobx';
-
-/* npm imports: material-ui/core */
-import CircularProgress from '@material-ui/core/CircularProgress';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
 
 /* root imports: view components */
-import { Icon } from 'views/elements';
+import { DrawerItem } from 'views/layouts/Drawer/DrawerItem';
 
 /* root imports: common */
 import { AuthStore } from 'features/Login/store';
@@ -22,25 +15,16 @@ interface LogoutProps {
 @inject('authStore')
 @observer
 class Logout extends React.Component<LogoutProps> {
-	@observable public isDrawerOpen: boolean = false;
-
-	@action public logoutHandler = () => {
-		this.props.authStore!.logout();
-	};
-
 	public render() {
-		const { inProgress } = this.props.authStore!;
+		const { inProgress, logout } = this.props.authStore!;
+
 		return (
-			<ListItem button onClick={this.logoutHandler}>
-				<ListItemIcon>
-					{inProgress ? (
-						<CircularProgress size={18} thickness={4} color="inherit" />
-					) : (
-						<Icon name="logout-variant" svgSize="md" />
-					)}
-				</ListItemIcon>
-				<ListItemText primary="Logout" />
-			</ListItem>
+			<DrawerItem
+				text="Logout"
+				iconName="logout-variant"
+				inProgress={inProgress}
+				onClick={logout}
+			/>
 		);
 	}
 }
--- src/views/layouts/Drawer/ThemeSwitcher/index.tsx
+++ src/views/layouts/Drawer/ThemeSwitcher/index.tsx
@@ -2,14 +2,8 @@
 import React from 'react';
 import { observer, inject } from 'mobx-react';
 
-/* npm imports: material-ui/core */
-import CircularProgress from '@material-ui/core/CircularProgress';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-
 /* root imports: view components */
-import { Icon } from 'views/elements';
+import { DrawerItem } from 'views/layouts/Drawer/DrawerItem';
 
 /* root imports: common */
 import { AuthStore } from 'features/Login/store';
@@ -25,16 +19,12 @@ class ThemeSwitcher extends React.Component<ThemeSwitcherProps> {
 		const { user, switchTheme, themeNameToSwitch } = this.props.authStore!;
 
 		return (
-			<ListItem button onClick={switchTheme}>
-				<ListItemIcon>
-					{user!.switchThemeInProgress ? (
-						<CircularProgress size={18} thickness={4} color="inherit" />
-					) : (
-						<Icon name="compare" svgSize="md" />
-					)}
-				</ListItemIcon>
-				<ListItemText primary={`Switch to ${themeNameToSwitch} theme`} />
-			</ListItem>
+			<DrawerItem
+				text={`Switch to ${themeNameToSwitch} theme`}
+				iconName="compare"
+				inProgress={user!.switchThemeInProgress}
+				onClick={switchTheme}
+			/>
 		);
 	}
 }
