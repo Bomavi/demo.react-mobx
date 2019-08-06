dac39fceb36c59d9e8bfc1c98b92b1b35e5529dd
Bomavi <max.bozh.n1mbo@gmail.com>
2019-08-06 21:43:10 +0300

    Cleanup views/elements

--- src/views/elements/InputButton/index.tsx
+++ src/views/elements/InputButton/index.tsx
@@ -15,52 +15,53 @@ import { useStyles } from './styles';
 export interface InputButtonProps {
 	icon: IconProps;
 	title?: string;
-	svgSize?: Sizes;
 	disabled?: boolean;
 	isFetching?: boolean;
 	color?: 'primary' | 'secondary' | 'inherit';
 	onClick?: () => void;
 }
 
-const InputButton: React.FC<InputButtonProps> = ({
-	icon: { name: iconName, svgSize = 'sm' },
-	title,
-	disabled,
-	isFetching = false,
-	color = 'inherit',
-	onClick,
-}) => {
-	const classes = useStyles();
+const InputButton: React.FC<InputButtonProps> = React.memo(
+	({
+		icon: { name: iconName, svgSize = 'sm' },
+		title,
+		disabled,
+		isFetching = false,
+		color = 'inherit',
+		onClick,
+	}: InputButtonProps) => {
+		const classes = useStyles();
 
-	if (isFetching) {
+		if (isFetching) {
+			return (
+				<div className={classes.root}>
+					<CircularProgress size={18} thickness={4} color="inherit" />
+				</div>
+			);
+		}
+
+		if (!onClick) {
+			return (
+				<div className={classes.root}>
+					<Icon name={iconName} size="sm" svgSize={svgSize} />
+				</div>
+			);
+		}
+
 		return (
 			<div className={classes.root}>
-				<CircularProgress size={18} thickness={4} color="inherit" />
+				<IconButton
+					className={cx(classes.iconButton, color)}
+					title={title}
+					color={color}
+					disabled={disabled}
+					onClick={onClick}
+				>
+					<Icon name={iconName} size="sm" svgSize={svgSize} />
+				</IconButton>
 			</div>
 		);
 	}
-
-	if (!onClick) {
-		return (
-			<div className={classes.root}>
-				<Icon name={iconName} size="sm" svgSize={svgSize} />
-			</div>
-		);
-	}
-
-	return (
-		<div className={classes.root}>
-			<IconButton
-				className={cx(classes.iconButton, color)}
-				title={title}
-				color={color}
-				disabled={disabled}
-				onClick={onClick}
-			>
-				<Icon name={iconName} size="sm" svgSize={svgSize} />
-			</IconButton>
-		</div>
-	);
-};
+);
 
 export { InputButton };
