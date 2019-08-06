b3dec249468ecc9945ba1f26d62e8e54dec67e27
Bomavi <max.bozh.n1mbo@gmail.com>
2019-08-06 21:50:51 +0300

    Cleanup features/Home/components

--- src/features/Home/components/SortButton/index.tsx
+++ src/features/Home/components/SortButton/index.tsx
@@ -16,25 +16,29 @@ export interface SortButtonProps {
 	onClick: () => void;
 }
 
-const SortButton: React.FC<SortButtonProps> = ({ sortKey, disabled, onClick }) => {
-	const classes = useStyles();
-	const isAsc = sortKey === 'asc';
-	const isDesc = sortKey === 'desc';
-	const newFirstTitle = 'Recently created first';
-	const oldFirstTitle = 'Oldest first';
+const SortButton: React.FC<SortButtonProps> = React.memo(
+	({ sortKey, disabled, onClick }) => {
+		const classes = useStyles();
 
-	return (
-		<div className={classes.root}>
-			<IconButton
-				title={isDesc ? oldFirstTitle : newFirstTitle}
-				disabled={disabled}
-				onClick={onClick}
-			>
-				{isAsc && <Icon name="sort-ascending" />}
-				{isDesc && <Icon name="sort-descending" />}
-			</IconButton>
-		</div>
-	);
-};
+		const isAsc = sortKey === 'asc';
+		const isDesc = sortKey === 'desc';
+
+		const newFirstTitle = 'Recently created first';
+		const oldFirstTitle = 'Oldest first';
+
+		return (
+			<div className={classes.root}>
+				<IconButton
+					title={isDesc ? oldFirstTitle : newFirstTitle}
+					disabled={disabled}
+					onClick={onClick}
+				>
+					{isAsc && <Icon name="sort-ascending" />}
+					{isDesc && <Icon name="sort-descending" />}
+				</IconButton>
+			</div>
+		);
+	}
+);
 
 export { SortButton };
