8f99b7795c4965d2af3ef5949c08a9d0108a3661
Bomavi <max.bozh.n1mbo@gmail.com>
2021-02-19 23:21:38 +0200

    Update Home feature components for hooks usage

--- src/features/Home/components/AddTask/index.tsx
+++ src/features/Home/components/AddTask/index.tsx
@@ -1,50 +1,30 @@
-/* npm imports: common */
-import { Component } from 'react';
-import { inject, observer } from 'mobx-react';
+import { FC } from 'react';
+import { observer } from 'mobx-react-lite';
 
-/* npm imports: material-ui/core */
-import { withStyles, WithStyles } from '@material-ui/core/styles';
-
-/* root imports: view components */
 import { CustomInput } from 'views/elements';
+import { useHomeStore } from 'features/Home/store';
 
-/* root imports: common */
-import { HomeStore } from 'features/Home/store';
+const AddTask: FC = observer(() => {
+	const { addTask, inProgress } = useHomeStore();
 
-/* local imports: common */
-import { styles } from './styles';
-
-interface AddTaskProps extends WithStyles<typeof styles> {
-	store?: HomeStore;
-}
-
-@inject('store')
-@observer
-class AddTaskComponent extends Component<AddTaskProps> {
-	public actionHandler = (value: string) => {
+	const actionHandler = (value: string) => {
 		if (value) {
-			this.props.store!.addTask({ description: value, completed: false });
+			addTask({ description: value, completed: false });
 		}
 	};
 
-	public render() {
-		const { inProgress } = this.props.store!;
-
-		return (
-			<CustomInput
-				icon={{
-					name: 'plus',
-					svgSize: 'md',
-					title: 'Add',
-				}}
-				isFetching={inProgress}
-				placeholder="Type task description..."
-				onClick={this.actionHandler}
-			/>
-		);
-	}
-}
-
-const AddTask = withStyles(styles)(AddTaskComponent);
+	return (
+		<CustomInput
+			icon={{
+				name: 'plus',
+				svgSize: 'md',
+				title: 'Add',
+			}}
+			isFetching={inProgress}
+			placeholder="Type task description..."
+			onClick={actionHandler}
+		/>
+	);
+});
 
 export { AddTask };
