7858d95f2e26eee6f8cd6e6f21316b111e0adc25
Bomavi <max.bozh.n1mbo@gmail.com>
2021-02-19 23:22:02 +0200

    Update Home feature components for hooks usage

--- src/features/Home/components/SortButton/index.tsx
+++ src/features/Home/components/SortButton/index.tsx
@@ -1,13 +1,9 @@
-/* npm imports: common */
-import * as React from 'react';
+import { FC, memo } from 'react';
 
-/* npm imports: material-ui/core */
 import IconButton from '@material-ui/core/IconButton';
 
-/* root imports: common */
 import { Icon } from 'views/elements';
 
-/* local imports: common */
 import { useStyles } from './styles';
 
 export interface SortButtonProps {
@@ -16,29 +12,27 @@ export interface SortButtonProps {
 	onClick: () => void;
 }
 
-const SortButton: React.FC<SortButtonProps> = React.memo(
-	({ sortKey, disabled, onClick }) => {
-		const classes = useStyles();
+const SortButton: FC<SortButtonProps> = memo(({ sortKey, disabled, onClick }) => {
+	const classes = useStyles();
 
-		const isAsc = sortKey === 'asc';
-		const isDesc = sortKey === 'desc';
+	const isAsc = sortKey === 'asc';
+	const isDesc = sortKey === 'desc';
 
-		const newFirstTitle = 'Recently created first';
-		const oldFirstTitle = 'Oldest first';
+	const newFirstTitle = 'Recently created first';
+	const oldFirstTitle = 'Oldest first';
 
-		return (
-			<div className={classes.root}>
-				<IconButton
-					title={isDesc ? oldFirstTitle : newFirstTitle}
-					disabled={disabled}
-					onClick={onClick}
-				>
-					{isAsc && <Icon name="sort-ascending" />}
-					{isDesc && <Icon name="sort-descending" />}
-				</IconButton>
-			</div>
-		);
-	}
-);
+	return (
+		<div className={classes.root}>
+			<IconButton
+				title={isDesc ? oldFirstTitle : newFirstTitle}
+				disabled={disabled}
+				onClick={onClick}
+			>
+				{isAsc && <Icon name="sort-ascending" />}
+				{isDesc && <Icon name="sort-descending" />}
+			</IconButton>
+		</div>
+	);
+});
 
 export { SortButton };
