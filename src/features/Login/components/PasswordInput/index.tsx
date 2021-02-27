1d06793d7154265f65c75f721692b5da73e723a3
Bomavi <max.bozh.n1mbo@gmail.com>
2021-02-28 00:10:38 +0200

    Update class component to functional

--- src/features/Login/components/PasswordInput/index.tsx
+++ src/features/Login/components/PasswordInput/index.tsx
@@ -1,20 +1,13 @@
-/* npm imports: common */
-import * as React from 'react';
+import { FC, memo } from 'react';
 
-/* npm imports: material-ui/core */
 import TextField, { TextFieldProps } from '@material-ui/core/TextField';
-
-/* local imports: common */
-// import { useStyles } from './styles';
 
 export interface PasswordInputProps {
 	repeatPassword?: boolean;
 }
 
-const PasswordInput: React.FC<PasswordInputProps & TextFieldProps> = React.memo(
+const PasswordInput: FC<PasswordInputProps & TextFieldProps> = memo(
 	({ repeatPassword, ...props }) => {
-		// const classes = useStyles();
-
 		const id = repeatPassword ? 'repeat-password' : 'password';
 		const label = repeatPassword ? 'Repeat password' : 'Password';
 
