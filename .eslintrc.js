b7efb414dc8cd5c74f2a08375a6e86da04319141
Bomavi <max.bozh.n1mbo@gmail.com>
2021-04-05 01:33:16 +0300

    Update configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,6 @@ module.exports = {
 	extends: [
 		'eslint:recommended',
 		'plugin:@typescript-eslint/recommended',
-		'prettier/@typescript-eslint',
 		'plugin:prettier/recommended',
 		// 'plugin:react/recommended',
 		'react-app',
